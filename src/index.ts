2ec8d3e81f3d7ac9ebd0baee5e5a9a5dae2f7161
monodyle <monodylh@gmail.com>
2024-01-20 14:28:40 +0700

    using sqrc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import { Resvg } from '@resvg/resvg-js'
 import http from 'node:http'
-import satori from 'satori'
-import { html } from 'satori-html'
-import QRCode from 'easyqrcodejs-nodejs'
 import { URL } from 'node:url'
-
-const logo = `<img src="https://images2.imgbox.com/be/0c/mpTLarKc_o.png" style="width:128px;height:128px;position:absolute;left:256px;top:256px;border-radius:128px;border:8px solid #fff;" />`
+import { QRCode } from 'sqrc'
 
 const requestListener = async function (
   req: http.IncomingMessage,
@@ -26,29 +21,29 @@ const requestListener = async function (
   res.setHeader('Cache-Control', 'max-age=31536000')
   res.writeHead(200)
 
-  var qrcode = new QRCode({
-    text: content,
-    width: 640 - 48,
-    height: 640 - 48,
-    correctLevel: QRCode.CorrectLevel.H,
-    quietZone: 24
+  const qr = new QRCode({
+    value: content,
+    logoImage: 'https://images2.imgbox.com/1b/48/FG6MDv15_o.png',
+    removeQrCodeBehindLogo: true,
+    size: 600,
+    logoWidth: 100,
+    quietZone: 10,
+    logoPadding: 4,
+    logoPaddingStyle: 'square',
+    ecLevel: 'M',
+    qrStyle: 'dots',
+    fgColor: '#1C1F25',
+    bgColor: '#FFF',
+    eyeRadius: [
+      [48, 48, 8, 48],
+      [48, 48, 48, 8],
+      [48, 8, 48, 48]
+    ]
   })
-  const data = await qrcode.toSVGText()
-  const template = html(
-    `<div style="width:640px;height:640px;position:relative;display:flex;">${data}${logo}</div>`
-  )
-  const svg = await satori(template, {
-    width: 640,
-    height: 640,
-    fonts: []
-  })
-  const resvg = new Resvg(svg, {
-    background: '#fff'
-  })
-  const pngData = resvg.render()
-  const pngBuffer = pngData.asPng()
 
-  res.end(pngBuffer)
+  const buffer = await qr.render()
+
+  res.end(buffer)
 }
 
 const server = http.createServer(requestListener)
